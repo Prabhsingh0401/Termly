e6b12bfb853a8aabc2f826bcb8771bffd7273021
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2026-06-30 00:56:03 +0530

    backend-fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,9 +14,13 @@ app.use(helmet());
 app.use(compression());
 app.use(morgan('dev'));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true,
-}));
+  origin: [
+    'https://termly-lac.vercel.app',
+    'https://termly-sigma.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}))
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
